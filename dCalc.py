7a5f52bb40416d5d9f5ab6087750fb9497083973
maaraa <maaraa@dotworld.in>
2018-03-11 12:26:58 +1100

    Completed Algorithm

---
+++ dCalc.py
@@ -0,0 +1,9 @@
+def checkDigitCalc(tacPlusSerial):
+    doubledResult = []
+    [doubledResult.append(int(str(int(x) * 2)[0]) + int(str(int(x) * 2)[1]) if len(str(int(x) * 2)) == 2 else int(x) * 2) if i % 2 != 0 else doubledResult.append(int(x)) for i, x in enumerate(str(tacPlusSerial))]
+    return int(str(tacPlusSerial) + str(10 - (sum(doubledResult) % 10)))
+
+tacPlusSerial = 35913906398494
+imei = checkDigitCalc(tacPlusSerial)
+print("TAC Plus Serial Number : ", tacPlusSerial)
+print("Calculated IMEI Number : ", imei)
